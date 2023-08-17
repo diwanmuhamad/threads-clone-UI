73f5d4adc9bb4283d71429993a968c663d831e21
Diwan <diwan.muhamad@nawadata.id>
2023-08-17 21:44:43 +0700

    add refresh config

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,20 +1,40 @@
-import { SafeAreaView, StyleSheet, ScrollView, Button } from "react-native";
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  Button,
+  Platform,
+} from "react-native";
 
 import EditScreenInfo from "../../components/EditScreenInfo";
 import { Text, View } from "../../components/Themed";
 import LottieView from "lottie-react-native";
 import { useRef } from "react";
+import { RefreshControl } from "react-native-gesture-handler";
 
 export default function TabOneScreen() {
-  const animation = useRef(null);
+  const animation = useRef<LottieView>(null);
 
   return (
-    <SafeAreaView style={styles.animationContainer}>
-      <ScrollView>
-        <Text>testingg</Text>
+    <SafeAreaView>
+      <ScrollView
+        contentContainerStyle={{
+          paddingHorizontal: 10,
+          paddingTop: Platform.select({ android: 30 }),
+        }}
+        refreshControl={
+          <RefreshControl
+            refreshing={false}
+            tintColor={"transparent"}
+            onRefresh={() => {
+              animation.current?.play();
+            }}
+          />
+        }
+      >
         <LottieView
           source={require("../../assets/animations/threads.json")}
-          loop={true}
+          loop={false}
           autoPlay
           ref={animation}
           style={{ width: 90, height: 90, alignSelf: "center" }}
@@ -23,12 +43,3 @@ export default function TabOneScreen() {
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  animationContainer: {
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    flex: 1,
-  },
-});
